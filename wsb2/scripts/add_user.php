7da321acb8c41e5a82c2ce3f309fe274996da6f0
simsonq123 <113249297+simsonq123@users.noreply.github.com>
2023-04-01 14:57:12 +0200

    Add files via upload

---
+++ wsb2/scripts/add_user.php
@@ -0,0 +1,19 @@
+<?php
+// print_r($_POST);
+
+foreach ($_POST as $key => $value){
+   // echo "$key: $value<br>";
+   if (empty($value)){
+  //  echo "$key<br>";
+  //echo "error";
+
+  echo "<script>history.back();</script>";
+  exit();
+   }
+}
+
+//echo "ok";
+require_once "./connect.php";
+$sql = "INSERT INTO `users` (`id`, `city_id`, `firstName`, `lastName`, `birthday`) VALUES (NULL, '$_POST[city_id]', '$_POST[firstName]', '$_POST[lastName]', '$_POST[birthday]');";
+$conn->query($sql);
+?>
---
+++ wsb2/scripts/delete_user.php
@@ -0,0 +1,20 @@
+<?php
+//print_r($_GET);
+//var_dump($_GET);
+
+
+require_once "./connect.php";
+ $sql ="DELETE FROM users WHERE `users`.`id` = $_GET[userIdDelete]";
+//$sql ="DELETE FROM users WHERE `users`.`firstname` = 'Janusz'";
+$conn->query($sql);
+
+echo $conn->affected_rows;
+
+if ($conn->query($sql) == 0){
+    header("location: ../3_db/4_db_table_delete.php?userDelete=0");
+    } else {
+        header("location: ../3_db/4_db_table_delete.php?userDelete=$_GET[userIdDelete]");
+    }
+
+
+?>
